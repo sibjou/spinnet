76c5d2118edb87a82b4c0ef9d484dc1ccd54a863
sibjou <veggie-grunt-going@duck.com>
2026-04-11 14:00:10 +0300

    Initial Commit

---
+++ Spinnet/SpinnetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinnetApp.swift
+//  Spinnet
+//
+//  Created by Mikhail on 11.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct SpinnetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
